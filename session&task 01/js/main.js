078a556d46c1917014695d30e20954c7e52e0636
MAIADA_MUHAMMED <166024947+MaiadaMuhammed@users.noreply.github.com>
2025-03-02 17:40:33 +0200

    Add files via upload

---
+++ session&task 01/js/main.js
@@ -0,0 +1,30 @@
+// 3- Operators
+
+
+// 1- Calculate the sum of two numbers and print the result
+let num1 = 8;
+let num2 = 22;
+let sum = num1 + num2;
+console.log("Sum:", sum); 
+
+// 2- Calculate the remainder when 15 is divided by 4
+let remainder = 15 % 4;
+console.log("Remainder:", remainder); 
+
+// 3- Multiply two numbers and print the result
+let product = 7 * 6;
+console.log("Product:", product);
+
+// 4- if "10" (String) is equal to 10 (Number)
+console.log('"10" == 10:', "10" == 10); 
+console.log('"10" === 10:', "10" === 10); 
+
+// 5- Condition checks with isStudent and age
+let isStudent = true;
+let age = 19;
+
+// Check if the person is a student AND their age is greater than 18
+console.log("Is student and age > 18:", isStudent && age > 18); 
+
+// Check if the person is a student OR their age is not greater than 18
+console.log("Is student or age not > 18:", isStudent || age <= 18); 
---
+++ session&task 01/js/script.js
@@ -0,0 +1,37 @@
+// 1: Variables Practice
+
+
+// 1- Create a variable using let to store my name
+let myName = "Maiada";
+
+// 2- Create a variable using const to store my age
+const myAge = 21;
+
+// 3- Use var to store whether I'm a student (true/false)
+var AmStudent = true;
+
+// 4- Print all the variables
+console.log("Name:", myName);
+console.log("Age:", myAge);
+console.log(" Am Student:",  AmStudent);
+
+
+
+// 2- Data Types
+
+
+// 1- an Object that contains My Name, age, and country
+let person = {
+    name: "Maiada",
+    age: 21,
+    country: "Egypt"
+};
+
+// 2- an Array with three of your favorite hobbies
+let hobbies = ["Coding", "listening To Podcast", "Cooking"];
+
+// 3- Print the second hobby in the array
+console.log("Second Hobby:", hobbies[1]);
+
+
+
