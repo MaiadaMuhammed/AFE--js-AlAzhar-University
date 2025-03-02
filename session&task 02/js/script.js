3f2f790cf64b455b7b48a69f0fbd5b5677e4c18b
MAIADA_MUHAMMED <166024947+MaiadaMuhammed@users.noreply.github.com>
2025-03-02 18:04:45 +0200

    Add files via upload

---
+++ session&task 02/js/script.js
@@ -0,0 +1,18 @@
+function sum(a, b) {
+    return a + b;
+}
+
+
+function showMessage() {
+    alert("Button: Thanks For Click me!");
+}
+
+// Example of how to use it in HTML:
+// <button onclick="showMessage()">Click Me</button>
+
+
+const logMessage = function() {
+    console.log("Mission Completed!.");
+};
+
+logMessage();
