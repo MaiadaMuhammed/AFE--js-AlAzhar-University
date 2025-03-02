ca2f0a395e99e2710e510ae17e1bc9af773b5b5c
MAIADA_MUHAMMED <166024947+MaiadaMuhammed@users.noreply.github.com>
2025-03-02 17:24:08 +0200

    Add files via upload

---
+++ session&task 02/js/script.js
@@ -0,0 +1,12 @@
+// 1. an Object that contains My Name, age, and country
+let person = {
+    name: "Maiada",
+    age: 21,
+    country: "Egypt"
+};
+
+// 2. an Array with three of your favorite hobbies
+let hobbies = ["Coding", "listening To Podcast", "Cooking"];
+
+// 3. Print the second hobby in the array
+console.log("Second Hobby:", hobbies[1]);
