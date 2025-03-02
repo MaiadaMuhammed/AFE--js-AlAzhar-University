8b6334c277b339fdc43410cbd553991a49203941
MAIADA_MUHAMMED <166024947+MaiadaMuhammed@users.noreply.github.com>
2025-03-02 18:06:17 +0200

    Add files via upload

---
+++ session&task 02/js/script.js
@@ -0,0 +1,15 @@
+function sum(a, b) {
+    return a + b;
+}
+
+
+function showMessage() {
+    alert("Button: Thanks For Click me!");
+}
+
+
+const logMessage = function() {
+    console.log("Mission Completed!.");
+};
+
+logMessage();
